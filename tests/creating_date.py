705440c573146665b451213a9228079e24cbbf6b
Alexey Orlov <96291255+Alexey92QA@users.noreply.github.com>
2022-12-21 15:37:53 +0300

    refactoring structures

---
+++ tests/creating_date.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+to_date = datetime.utcnow().strftime("%Y-%m-%d")
+fail_date = datetime(1900, 12, 4).strftime("%Y-%m-%d")
---
+++ tests/credentials.py
@@ -0,0 +1,2 @@
+base_url = 'https://api.nasa.gov/planetary/apod'
+auth = 'faJPYb2zCdNTA8C0ZV5An26T0sV5lif8z4DG8IEh'
